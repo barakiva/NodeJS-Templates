022d62344af09d6a6059aea271aeefad1f16ce82
ratchet <barakiva1@gmail.com>
2022-07-15 20:34:08 +0300

    basic express routing

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,18 @@
 import express from "express";
 const app = express();
-const port = 3001;
-app.get('/', (req, res) => {
-    res.send("Hello template")
-})
+const homeRoute = express.Router();
+const userRoute = express.Router();
+const port = 3009;
 
+//Routing
+homeRoute.get('/home', function (req, res) {
+	res.send("Hello /hello")
+})
+userRoute.get('/user', function (req, res) {
+	res.send('Hello user')
+})
+app.use('/', homeRoute)
+app.use('/', userRoute)
 app.listen(port, (req, res) => {
-    console.info(`Listening on port ${port}`)
+		console.info(`Listening on port ${port}`)
 })
