9e25ad93fab96734a7eede1cd0db9f53f8dadc7a
ratchet <barakiva1@gmail.com>
2022-07-16 10:27:24 +0300

    converted to lambdas

--- src/app/index.js
+++ src/app/index.js
@@ -5,14 +5,18 @@ const userRoute = express.Router();
 const port = 3009;
 
 //Routing
-homeRoute.get('/home', function (req, res) {
-	res.send("Hello /hello")
+//	Home
+homeRoute.get('/home', (req, res) => {
+	res.redirect('/user')
 })
-userRoute.get('/user', function (req, res) {
+//	Users
+userRoute.get('/user', (req, res) => {
 	res.send('Hello user')
 })
+// Routes
 app.use('/', homeRoute)
 app.use('/', userRoute)
+//Server
 app.listen(port, (req, res) => {
 		console.info(`Listening on port ${port}`)
 })
