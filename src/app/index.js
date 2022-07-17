f471ee121f0534756eb8d4379c212b341f146c52
Bar Akiva <barakiva1@gmail.com>
2022-07-17 13:20:13 +0300

    basic post with HTTP status and JSON body

--- src/app/index.js
+++ src/app/index.js
@@ -7,9 +7,16 @@ let port;
 await helper.findFreePort.then((res)=> port = res)
 
 app.get('/', (req, res) => {
-    res.send("Hello template")
+	res
+		.status(200)
+		.send('Hello there!')
+})
+app.post('/', (req, res)=> {
+	res
+		.status(200)
+		.send({msg: 'Hello there', sender: "Bar"})
 })
 
 app.listen(port, (req, res) => {
-    console.info(`Listening on port ${port}`)
+	console.info(`Listening on port ${port}`)
 })
