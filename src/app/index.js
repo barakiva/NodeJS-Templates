57775057a626220401aaac379ff2ca1c49a1386b
Bar Akiva <barakiva1@gmail.com>
2022-07-17 19:15:44 +0300

    can read request body

--- src/app/index.js
+++ src/app/index.js
@@ -5,7 +5,7 @@ import helper from "./helper.js";
 const app = express();
 let port;
 await helper.findFreePort.then((res)=> port = res)
-
+app.use(express.json())
 app.get('/', (req, res) => {
 	res
 		.status(200)
@@ -15,6 +15,12 @@ app.post('/', (req, res)=> {
 	res
 		.status(200)
 		.send({msg: 'Hello there', sender: "Bar"})
+})
+app.post('/authenticate', (req, res)=> {
+	console.dir(req.body)
+	res
+		.status(200)
+		.send({msg: 'Authenticated properly!', sender: "Bar"})
 })
 
 app.listen(port, (req, res) => {
