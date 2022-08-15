e53287db8cb2e4a624a8a53549383b1cbefd1784
Bar Akiva <barakiva1@gmail.com>
2022-08-15 12:07:33 +0300

    all endpoints work with mongodb

--- src/app/index.js
+++ src/app/index.js
@@ -1,58 +1,59 @@
 import express from "express";
 import mongoose from "mongoose";
-
 import Blog from "./blog.js"
+
 const app = express();
 const port = 3001;
 
 // Middleware
+app.use(express.json())
 function addBlog(req, res, next) {
-    const blog = new Blog({ 
-        title: 'new blog',
-        snippet: 'about my new blog',
-        body: 'ohh touch my body'
-    });
-    blog.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.error(err)
-        })
+	const blog = new Blog({ 
+		title: req.body.title,
+		snippet: req.body.snippet,
+		body: req.body.body
+	});
+	blog.save()
+		.then((data) => {
+			res.send(data)
+		})
+		.catch((err) => {
+			console.error(err)
+		})
 }
 function getAllBlogs (req, res) {
-    Blog.find()
-        .then((result) => {
-            res.send(result)
-        })
+	Blog.find()
+		.then((data) => {
+			res.send(data)
+		})
 }
 function getBlogById(req, res) {
-    Blog.findById(req.params.id)
-        .then((result) => {
-            res.send(result)
-        })
+	Blog.findById(req.params.id)
+		.then((data) => {
+			res.send(data)
+		})
 }
 // Express routes
-app.get('/add-blog', addBlog)
+app.post('/add-blog', addBlog)
 app.get('/all-blogs', getAllBlogs)
 app.get('/single-blog/:id', getBlogById )
 // Database
-mongoose.connect('mongodb://localhost:27017/test')
-    .then((res) => app.listen(port, 
-        ()=> console.info(`Listening on port ${port}`)))
-    .catch((err) => console.error(err))
+mongoose
+	.connect('mongodb://localhost:27017/test')
+	.then((res) => app.listen(port, ()=> console.info(`Listening on port ${port}`)))
+	.catch((err) => console.error(err))
 
 // Schemas
 const kittySchema = new mongoose.Schema({
-    name: String
+	name: String
 })
 const Kitten = mongoose.model('Kitten', kittySchema)
 const silence = new Kitten({ name: 'Silence' });
 console.log(silence.name); // 'Silence'
 
 kittySchema.methods.speak = function speak() {
-    const greeting = this.name
-      ? "Meow name is " + this.name
-      : "I don't have a name";
-    console.log(greeting);
+	const greeting = this.name
+	  ? "Meow name is " + this.name
+	  : "I don't have a name";
+	console.log(greeting);
 };
