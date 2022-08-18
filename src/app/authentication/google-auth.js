63b92afad8c02356fc4c4dc64b5d2a6e9b04cd90
Bar Akiva <barakiva1@gmail.com>
2022-08-18 10:50:08 +0300

    google auth successful

---
+++ src/app/authentication/google-auth.js
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv'
+import passport from 'passport'
+import googleStrategy from 'passport-google-oauth20'
+//Config
+dotenv.config()
+
+passport.use(new googleStrategy.Strategy({
+	clientID: process.env.GOOGLE_CLIENT_ID,
+	clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	callbackURL: "http://localhost:5000/google/callback",
+	passReqToCallback: true
+	},
+	function(accessToken, refreshToken, profile, done) {
+		return done(null, profile)
+	}
+))
+
+passport.serializeUser(function(user,done){
+	done(null, user)
+})
+passport.deserializeUser(function(user,done){
+	done(null, user)
+})
--- src/app/index.js
+++ src/app/index.js
@@ -1,12 +1,18 @@
-import express from "express";
+import express, { application } from "express";
 import session from 'express-session'
 import {fileURLToPath} from 'url';
 import {dirname, join} from 'path';
 import mongoose from "mongoose";
 import loginRoutes from './login.js';
 import BearerRoutes from './routes/bearer-routes.js'
+import dotenv from 'dotenv'
+import passport from 'passport'
+import { resolveNaptr } from "dns";
+import './authentication/google-auth.js'
+//Config
+dotenv.config()
 const app = express();
-const port = 3009;
+const port = process.env.PORT;
 //Static
 const __dirname = dirname(fileURLToPath(import.meta.url))
 app.use(express.static(join(__dirname, 'public/')))
@@ -17,13 +23,41 @@ app.use(session({
 	saveUninitialized: true,
 	secret: 'bla bla bla' 
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 // Credentials
 app.use('/', loginRoutes)
+app.get('/', (req, res)=> {
+	res.send("Welcome to passport auth lab!")
+})
 app.get('login', (req, res)=> {
 	res.sendFile('/login.html')
 })
 // Bearer Token
 app.use('/', BearerRoutes)
+// Google Login
+app.get('/auth/google',  // Choose an account to continue to AuthTest
+	passport.authenticate('google', { scope: ['email', 'profile']
+}))
+app.get('/auth/google/callaback',
+	passport.authenticate('google', {
+		successRedirect: '/protected',
+		failureRedirect: '/auth/google/failure'
+	}
+))
+// Protected routes
+function isLoggedIn(req, res, next) {
+	req.user ? next() : res.sendStatus(401)
+}
+app.get('/auth/google/failure', (req, res)=> {
+	res.send('Something went wrong')
+})
+app.get('/protected', isLoggedIn, (req, res)=> {
+	res.send("You're in!")
+})
+app.get('/google/callback', (req,res)=> {
+	res.send('Successful authentication!')
+})
 //Server
 mongoose
 	.connect('mongodb://localhost:27017/test')
