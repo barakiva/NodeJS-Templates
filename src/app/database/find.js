adeee0ab19cd6a0eb3b4d72bc83a5dfc87e267e7
Bar Akiva <barakiva1@gmail.com>
2022-09-19 17:00:56 +0300

    moar good stuff

--- src/app/database/find.js
+++ src/app/database/find.js
@@ -1,18 +1,16 @@
-import mongoose from 'mongoose'
 import Blog from './../schemas/blog.js'
-
-
 
 export async function init() {
     console.log('Using then')
-    // Blog.findOne({title: 'new blog'})
-    //     .then((res)=> console.log(res))
-    // console.log('Using a callback')
-    // Blog.findOne({title: 'new blog'}, function(err, data) {
-    //     if (err) console.log(err)
-    //     if (data) console.log(data)
-    // })
+    Blog.findOne({title: 'new blog'})
+        .then((res)=> console.log(res))
+    console.log('Using a callback')
+    Blog.findOne({title: 'new blog'}, function(err, data) {
+        console.log(`Error: ${err} and ${data}`)
+        if (err) console.log(err)
+        if (data) console.log(data)
+    })
     console.log('Using await')
-    const res = await Blog.findOne({title: 'new blog'});
+    const res = await Blog.findOne({title: 'new blog'}).exec();
     console.log(`await find is ... ${res}`)
 }
